382dcc0215a4067524f4f0dc5ce020ba54473ee9
Metin ÇETİN <metincetin_1996@gmail.com>
2021-12-19 23:23:37 +0300

    Action Types Has Been Updated

--- frontend/src/redux/actions/actionTypes.js
+++ frontend/src/redux/actions/actionTypes.js
@@ -1,3 +1,4 @@
+//AUTH ACTIONS
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
@@ -7,3 +8,7 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
+
+//PRODUCT ACTIONS
+export const PRODUCT_LOADED = "PRODUCT_LOADED"
+export const PRODUCT_LOADED_FAIL = "PRODUCT_LOADED_FAIL"
