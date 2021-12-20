52c28c0be10f9c093432710a944c367b5603a394
Metin ÇETİN <metincetin_1996@gmail.com>
2021-12-20 19:22:43 +0300

    Company Reducer Has Been Added

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import product from "./product"
+import company from "./company"
 
 export default combineReducers({
   alert,
   auth,
-  product
+  product,
+  company
 });
